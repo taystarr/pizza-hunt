3f0ae521f4a20ee0941e669f672a3a2a73259f66
Taylor Roberts <tstarrroberts@gmail.com>
2022-06-16 17:29:05 -0400

    add comments ability

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema({
     pizzaName: {
@@ -9,13 +10,34 @@ const PizzaSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
         type: String,
         default: 'Large'
     },
-    toppings: []
+    toppings: [],
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+},
+{
+    // need to tell schema it can use virtuals
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+}
+);
+
+// get total count of comments and replies using virtuals
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
 });
 
 // create Pizza model using PizzaSchema
