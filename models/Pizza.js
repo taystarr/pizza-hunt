8cae667ba7577b547356573c875a7c2ba6dc8c2a
Taylor Roberts <tstarrroberts@gmail.com>
2022-06-21 05:37:04 -0400

    add replies functionality

--- models/Pizza.js
+++ models/Pizza.js
@@ -37,7 +37,7 @@ const PizzaSchema = new Schema({
 
 // get total count of comments and replies using virtuals
 PizzaSchema.virtual('commentCount').get(function() {
-    return this.comments.length;
+    return this.comments.reduce((total, comment) => total + comment.replies.length +1, 0);
 });
 
 // create Pizza model using PizzaSchema
